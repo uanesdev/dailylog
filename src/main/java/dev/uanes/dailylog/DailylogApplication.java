315771ce33b8052640de62b1ecfe70459fa99276
Uanes <uanesfrancisco@gmail.com>
2026-06-22 10:27:54 -0300

    first commit

---
+++ src/main/java/dev/uanes/dailylog/DailylogApplication.java
@@ -0,0 +1,13 @@
+package dev.uanes.dailylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailylogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/uanes/dailylog/DailylogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.uanes.dailylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
